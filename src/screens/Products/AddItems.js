e7c753613e5c0cd4dfe782fd28e9badc5209e78c
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2021-11-06 13:07:26 +0200

    Styled the product screens

--- src/screens/Products/AddItems.js
+++ src/screens/Products/AddItems.js
@@ -1,7 +1,22 @@
+import { Container, Row, Col } from "react-bootstrap"
+import { Link, useParams } from "react-router-dom"
+
+// Import Components
+import Navigation from "../../components/Navigation"
+
 const AddItems = () => {
   return (
     <div>
-      <h1>Item Added...</h1>
+      <Navigation />
+
+      <Container>
+        <Row>
+          <Col className="d-flex flex-column justify-content-center mt-5">
+            <h2>A new item was added! </h2>
+            <Link to="/products">Go Back To Products</Link>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
--- src/screens/Products/SpecificProduct.js
+++ src/screens/Products/SpecificProduct.js
@@ -1,7 +1,24 @@
+import { Container, Row, Col } from "react-bootstrap"
+import { Link, useParams } from "react-router-dom"
+
+// Import Components
+import Navigation from "../../components/Navigation"
+
 const SpecificProduct = () => {
+  const { id } = useParams()
+
   return (
     <div>
-      <h1>Here we Are!</h1>
+      <Navigation />
+
+      <Container>
+        <Row>
+          <Col className="d-flex flex-column justify-content-center mt-5">
+            <h2>This is product with id {id} </h2>
+            <Link to="/products">Go Back To Products</Link>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
